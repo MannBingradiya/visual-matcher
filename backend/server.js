687d2ba4f41cb372f1aea1b1ff7a5b206eecc40d
Man Bhingradiya <137755061+MannBingradiya@users.noreply.github.com>
2025-10-15 15:47:48 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -30,6 +30,10 @@ app.use(cors({
 app.use(express.json({ limit: "50mb" }));
 app.use(express.urlencoded({ extended: true }));
 
+app.use((req, res, next) => {
+  console.log(`➡️  ${req.method} ${req.url}`);
+  next();
+});
 
 app.use("/api/search", searchRoutes);
 
