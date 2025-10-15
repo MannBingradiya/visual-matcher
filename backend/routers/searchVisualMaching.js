becf0c6470ee2de2c4117a179ddd0de3b649a39d
Man Bhingradiya <137755061+MannBingradiya@users.noreply.github.com>
2025-10-15 12:33:52 +0530

    Update searchVisualMaching.js

--- backend/routers/searchVisualMaching.js
+++ backend/routers/searchVisualMaching.js
@@ -1,12 +1,11 @@
 import express from "express";
 import { upload } from "../middlewares/uploadMiddleware.js";
-// import { searchProducts } from "../controllers/searchController.js";
-import { searchImage } from "../controllers/searchController.js";
+import { searchProducts } from "../controllers/searchController.js";
 
 const router = express.Router();
 
-// router.post("/", upload.single("imageFile"), searchProducts);
+router.post("/", upload.single("imageFile"), searchProducts);
 
-router.post("/search", upload.single("image"), searchImage);
+// router.post("/search", upload.single("image"), searchImage);
 
 export default router;
